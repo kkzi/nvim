c1b0c31395bd602a1af35439c094f3a9b3fac4e4
gx <xuan.guo@atomdatatech.com>
2024-08-25 10:47:01 +0800

    use nvchad

---
+++ lua/configs/nvimtree.lua
@@ -0,0 +1,15 @@
+require("nvchad.configs.nvimtree").defaults()
+
+-- local nvimtree = require "nvimtree"
+-- nvimtree.options = {
+--   prefer_startup_root = true,
+--   sync_root_with_cwd = true,
+--   reload_on_bufenter = true,
+-- respect_buf_cwd=true,
+--   update_focused_file = {
+--     enable = true,
+--     update_root = {
+--       enable = true,
+--     },
+--   },
+-- }
---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.guifont = "Sarasa Fixed SC Nerd Font:h13"
+o.autochdir = true
+o.cursorline = true
+o.relativenumber = true
+o.cursorlineopt = "both"
