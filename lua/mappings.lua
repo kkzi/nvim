81148b000070e699b1b3e4c4fdf0b1831b47b8cb
gx <xuan.guo@atomdatatech.com>
2024-09-11 22:52:18 +0800

    add C-s

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,13 +1,6 @@
 local map = vim.keymap.set
 local nomap = vim.keymap.del
 
--- override
--- nomap("n", "<leader>h")
--- nomap("n", "<leader>v")
--- nomap("n", "<leader>n")
--- nomap("n", "<leader>b")
--- nomap({ "n", "t" }, "<A-i>")
-
 map("n", "<ESC>", "<CMD>nohl<CR>", { desc = "clear hilights" })
 
 -- map("i", "jk", "<esc>")
@@ -20,14 +13,12 @@ map("n", "<A-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<A-j>", "<C-w>j", { desc = "switch window down" })
 map("n", "<A-k>", "<C-w>k", { desc = "switch window up" })
 
-map({ "c", "i" }, "<S-Insert>", "<C-r>+", {})
+map({ "c", "i" }, "<S-Insert>", "<C-r>+")
 map({ "c", "i" }, "<C-v>", "<C-r>+", {})
+map({ "n", "i", "v" }, "<C-s>", "<CMD>w<CR>", {})
 
 -- preference
 map("n", "<leader>zp", "<cmd>:e $MYVIMRC<CR>", { desc = "Preferences" })
-map("n", "<leader>zk", "<cmd>Telescope keymaps<CR>", { desc = "Shortcuts" })
-map("n", "<leader>zc", "<cmd>cd %:p:h<CR>", { desc = "Cd current buffer file dir" })
 
 -- c++
 -- map({ "i", "n" }, "<M-o>", "<CMD>ClangdSwitchSourceHeader<CR>")
--- map({ "n, "i", "v" }, "<C-s>", "<cmd> w <cr>")
